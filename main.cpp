92d0565785fdf44f483a521046e2828cebe4ebc2
Alik <A@A.mail>
2024-12-11 01:03:08 +0200

    Initial project version

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cpp1
+//
+//  Created by Alik on 05.11.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, gjjgjgjgjgjgWorld!\n";
+    return 0;
+}
